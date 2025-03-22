40824dc490d9171d43eaf59849639125da6fd62f
Stuarth195 <167147559+Stuarth195@users.noreply.github.com>
2025-03-22 17:39:40 -0600

    commit inicial

---
+++ CA3/ColaLista.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using EstructuraDeDatos;
+
+namespace EstructuraDeDatos
+{
+    public class ColaLista<T> // Corregí el nombre
+    {
+        private Nodo<T> Frente;
+        private Nodo<T> Final;
+        private int Largo;
+
+        public ColaLista()
+        {
+            Frente = null;
+            Final = null;
+            Largo = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return Largo == 0;
+        }
+
+        public void Encolar(T dato)
+        {
+            Nodo<T> nuevoNodo = new Nodo<T>(dato);
+            if (IsEmpty())
+            {   
+                Frente = nuevoNodo;
+                Final = nuevoNodo;
+            }
+            else
+            {
+                Final.Siguiente = nuevoNodo;
+                Final = nuevoNodo;
+            }
+            Largo++;
+        }
+
+        public void EnQueQue(T Item) // Este método no se comporta como una cola FIFO
+        {
+            Nodo<T> NuevoNodo = new Nodo<T>(Item);
+            if (IsEmpty())
+            {
+                Frente = NuevoNodo;
+                Final = NuevoNodo;
+            }
+            else
+            {
+                NuevoNodo.Siguiente = Frente;
+                Frente = NuevoNodo;
+            }
+        }
+    }
+}
---
+++ CA3/Nodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EstructuraDeDatos
+{
+    public class Nodo<T>
+    {
+            public T Dato { get; set; }
+            public Nodo<T> Siguiente { get; set; }
+        public Nodo(T dato)
+        {
+            Dato = dato;
+            Siguiente = null;
+        }
+    }
+}
